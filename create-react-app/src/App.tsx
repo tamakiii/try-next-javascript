2c8ae1cbcff814de1835e4743dc7bb5e6416d0f8
tamakiii <tamaki.htm@gmail.com>
2018-07-16 16:38:57 +0900

    add Hello component

--- create-react-app/src/App.tsx
+++ create-react-app/src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './App.css';
+import Hello from './components/Hello';
 
 import logo from './logo.svg';
 
@@ -14,6 +15,7 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Hello name="TypeScript" enthusiasmLevel={10} />
       </div>
     );
   }
