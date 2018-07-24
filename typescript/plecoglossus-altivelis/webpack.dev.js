c1b2e296266cee1bb650962b297bf3600b5afd05
tamakiii <tamaki.htm@gmail.com>
2018-07-24 22:02:17 +0900

    tab -> space

--- typescript/plecoglossus-altivelis/webpack.dev.js
+++ typescript/plecoglossus-altivelis/webpack.dev.js
@@ -43,82 +43,82 @@ const precss = require('precss');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				include: [path.resolve(__dirname, 'src')],
-				loader: 'babel-loader',
+  module: {
+    rules: [
+      {
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader',
 
-				options: {
-					plugins: ['syntax-dynamic-import'],
+        options: {
+          plugins: ['syntax-dynamic-import'],
 
-					presets: [
-						[
-							'env',
-							{
-								modules: false
-							}
-						]
-					]
-				},
+          presets: [
+            [
+              'env',
+              {
+                modules: false
+              }
+            ]
+          ]
+        },
 
-				test: /\.js$/
-			},
-			{
-				test: /\.css$/,
+        test: /\.js$/
+      },
+      {
+        test: /\.css$/,
 
-				use: [
-					{
-						loader: 'style-loader'
-					},
-					{
-						loader: 'css-loader',
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
 
-						options: {
-							importLoaders: 1,
-							sourceMap: true
-						}
-					},
-					{
-						loader: 'postcss-loader',
+            options: {
+              importLoaders: 1,
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
 
-						options: {
-							plugins: function() {
-								return [precss, autoprefixer];
-							}
-						}
-					}
-				]
-			}
-		]
-	},
+            options: {
+              plugins: function () {
+                return [precss, autoprefixer];
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
 
-	entry: {
-		era: 'src/era.js',
-		kama: 'src/kama.js',
-		zeigo: 'src/zeigo.js'
-	},
+  entry: {
+    era: 'src/era.js',
+    kama: 'src/kama.js',
+    zeigo: 'src/zeigo.js'
+  },
 
-	output: {
-		filename: '[name].[chunkhash].js',
-		path: path.resolve(__dirname, 'dist')
-	},
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
 
-	mode: 'development',
+  mode: 'development',
 
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/
-				}
-			},
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
 
-			chunks: 'async',
-			minChunks: 1,
-			minSize: 30000,
-			name: true
-		}
-	}
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: true
+    }
+  }
 };
