5341d0899a18fc48e572f46c194955813d58b16b
tamakiii <tamaki.htm@gmail.com>
2018-07-25 10:09:21 +0900

    fix spaces

--- typescript/siganus-fuscescens/webpack.prod.js
+++ typescript/siganus-fuscescens/webpack.prod.js
@@ -44,71 +44,71 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
  */
 
 module.exports = {
-   module: {
-      rules: [
-         {
-            include: [path.resolve(__dirname, 'src')],
-            loader: 'babel-loader',
+  module: {
+    rules: [
+      {
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader',
+
+        options: {
+          plugins: ['syntax-dynamic-import'],
+
+          presets: [
+            [
+              'env',
+              {
+                modules: false
+              }
+            ]
+          ]
+        },
+
+        test: /\.js$/
+      },
+      {
+        test: /\.css$/,
+
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
 
             options: {
-               plugins: ['syntax-dynamic-import'],
-
-               presets: [
-                  [
-                     'env',
-                     {
-                        modules: false
-                     }
-                  ]
-               ]
-            },
-
-            test: /\.js$/
-         },
-         {
-            test: /\.css$/,
-
-            use: [
-               {
-                  loader: MiniCssExtractPlugin.loader
-               },
-               {
-                  loader: 'css-loader',
-
-                  options: {
-                     importLoaders: 1,
-                     sourceMap: true
-                  }
-               },
-               {
-                  loader: 'postcss-loader',
-
-                  options: {
-                     plugins: function() {
-                        return [precss, autoprefixer];
-                     }
-                  }
-               }
-            ]
-         }
-      ]
-   },
-
-   mode: 'production',
-
-   optimization: {
-      splitChunks: {
-         cacheGroups: {
-            vendors: {
-               priority: -10,
-               test: /[\\/]node_modules[\\/]/
+              importLoaders: 1,
+              sourceMap: true
             }
-         },
+          },
+          {
+            loader: 'postcss-loader',
 
-         chunks: 'async',
-         minChunks: 1,
-         minSize: 30000,
-         name: false
+            options: {
+              plugins: function() {
+                return [precss, autoprefixer];
+              }
+            }
+          }
+        ]
       }
-   }
+    ]
+  },
+
+  mode: 'production',
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
+
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: false
+    }
+  }
 };
